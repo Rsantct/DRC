72dd134524f4dbc0e64e4abeb592b1e1fd9620b6
Rsantct <rsantct@gmail.com>
2018-09-01 14:24:56 +0200

    Update synthSweep.py

--- swetsine/synthSweep.py
+++ swetsine/synthSweep.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 """
-                       NO ACABADO TOAVIA
+                       NO ACABADO TODAVIA
               
 ######################################################################################
 ## Adaptación a Python del original:
@@ -71,7 +71,7 @@ Gd_start = np.ceil( min( N/10.0, max( FS/f1, N/200.0) ) )
 ##
 ## %%% set fadeout length
 ## postfade = ceil(min(N/10,max(FS/f2,N/200)));
-postfade = np.ceil( min( N/10.0, max( FS/f2, N/200.0) ) )
+postfade = int( np.ceil( min( N/10.0, max( FS/f2, N/200.0) ) ) )
 ##
 ## %%% find the length of the actual sweep when its between f1 and f2
 ## Nsweep = N - tail - Gd_start - postfade;
@@ -96,9 +96,8 @@ f = np.linspace(0, FS/2.0, N+1)      # es  ODD N+1 con el bin zero
 ## %%% create pink (-10dB per decade, or 1/(sqrt(f)) spectrum
 ## mag = [sqrt(f1./f(1:end))];
 ## mag(1) = mag(2);
-mag = np.sqrt( f1 /f)           # esto da un warning pero el resultado es igual que
-                                # la expresion de Matlab, el primer bin es 'inf'
-mag[0] = mag[1]                 # apaño para el 'inf'
+mag = np.sqrt( f1 /f)   # warning porque mag[0] => 'inf', lo solucionamos
+mag[0] = mag[1]         # copiando el valor del bin siguiente.
 ##
 ## %%% Create band pass magnitude to start and stop at desired frequencies
 ## [B1 A1] = butter(2,f1/(FS/2),'high' );  %%% HP at f1
@@ -107,17 +106,16 @@ B1, A1 = signal.butter(2, f1/(FS/2.0), 'high')  # LP at f1
 B2, A2 = signal.butter(2, f2/(FS/2.0), 'low')   # LP at f1
 ##
 ## %%% convert filters to freq domain
-## [H1 W1] = freqz(B1, A1, N+1, FS);
-## [H2 W2] = freqz(B2, A2, N+1, FS);
-W1, H1 = signal.freqz(B1, A1, worN=(N+1))   # (i) freqz en Scipy no usa la FS
-W2, H2 = signal.freqz(B2, A2, worN=(N+1))
-W1 = W1 / (2*np.pi) * FS                    # frecs normalizadas --> físicas
+## [H1 W1] = freqz(B1, A1, N+1, FS);        # (i) Matlab calcula las freq físicas
+## [H2 W2] = freqz(B2, A2, N+1, FS);        #     si se le indica la FS, pero 
+W1, H1 = signal.freqz(B1, A1, worN=(N+1))   #     Scipy no usa la FS. Hay que calcular
+W2, H2 = signal.freqz(B2, A2, worN=(N+1))   #     aparte las freq físicas.
+W1 = W1 / (2*np.pi) * FS                    #  freq normalizadas --> físicas
 W2 = W2 / (2*np.pi) * FS
 ##
 ## %%% multiply mags to get final desired mag spectrum
 ## mag = mag.*abs(H1)'.*abs(H2)';
 mag = mag * np.abs(H1) * np.abs(H2)
-
 
 ##
 ##
@@ -128,13 +126,13 @@ mag = mag * np.abs(H1) * np.abs(H2)
 ## %%% calc group delay for arbitrary mag spectrum with contant time envelope
 ## %%% from Muller eq's 11 and 12
 ## C = tsweep ./ sum(mag.^2);
-C = tsweep / np.sum( mag**2 )
 ## Gd = C * cumsum(mag.^2);
-Gd = C * np.cumsum( mag**2 )
 ## Gd = Gd + Gd_start/FS;       % add predelay
-Gd = Gd + Gd_start/FS
 ## Gd = Gd*FS/2;                % convert from secs to samps
-Gd = Gd*FS/2.0
+C  = tsweep / np.sum( mag**2 )
+Gd = C * np.cumsum( mag**2 )
+Gd = Gd + Gd_start/FS
+Gd = Gd * FS/2.0
 
 ##
 ## %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -167,7 +165,7 @@ ph = ph - (f/(FS/2)) * ( ph[-1] % (2*np.pi) )
 cplx = mag * np.exp( (0+1j) * ph )
 ## %%% conjugate, flip, append for WHOLE SPECTRUM
 ## cplx = [ cplx conj( fliplr( cplx(2:end-1 ) ) ) ];
-cplxR = np.conj( np.flipud( cplx[1:-1] ) )              # el vector python es con flipud
+cplxR = np.conj( np.flipud( cplx[1:-1] ) )              # aquí con flipud
 cplx = np.concatenate( [cplx, cplxR] )
 ##
 ## %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -192,27 +190,36 @@ err = np.max( np.abs( np.imag ( np.fft.ifft(cplx) ) ) )
 ## w = hann(2*Gd_start)';
 ## I = 1:Gd_start;
 ## ir(I) = ir(I).*w(I);
-w = np.hanning( 2 * Gd_start )
-I = np.arange(1, Gd_start+1)
+w = np.hanning( 2*Gd_start )
+I = np.arange(Gd_start, dtype=int)  # OjO índices Pythpn desde cero
+ir[I] = ir[I] * w[I]
 ##
 ## %%% create window for fade-out and apply
 ## w = hann(2*postfade)';
 ## I = Gd_start+Nsweep+1:Gd_start+Nsweep+postfade;
 ## ir(I) = ir(I).*w(postfade+1:end);
+w = np.hanning( 2*postfade)
+I = np.arange(Gd_start+Nsweep, Gd_start+Nsweep+postfade, dtype=int)
+ir[I] = ir[I] * w[postfade:]
 ##
 ## %%% force the tail beyond the fadeout to zeros
 ## I = Gd_start+Nsweep+postfade+1:length(ir);
 ## ir(I) = zeros(1,length(I));
+I = np.arange(Gd_start+Nsweep+postfade, len(ir), dtype=int)
+ir[I] = np.zeros(len(I))
 ##
 ## %%% cut the sweep down to its correct size
 ## ir = ir(1:end/2);
+ir = ir[:len(ir)/2]
 ##
 ## %%% normalize
 ## ir = ir/(max(abs(ir(:))));
+ir = ir / max( abs(ir) )
 ##
 ##
 ## %%% get fft of sweep to verify that its okay and to use for inverse
 ## irfft = fft(ir);
+irfft = np.fft.fft(ir)
 ##
 ##
 ## %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -223,14 +230,25 @@ I = np.arange(1, Gd_start+1)
 ## %%% this includes the band-pass filtering, whos inverse could go to
 ## %%% infinity!!!
 ## invirfft = 1./irfft;
+invirfft = 1/irfft
 ##
 ## %%% so we need to re-apply the band pass here to get rid of that
 ## [H1 W1] = freqz(B1,A1,length(irfft),FS,'whole');
 ## [H2 W2] = freqz(B2,A2,length(irfft),FS,'whole');
+## [H1 W1] = freqz(B1,A1,length(irfft),'whole',FS); # (!!!) Operandos mal colocados
+## [H2 W2] = freqz(B2,A2,length(irfft),'whole',FS);
+W1, H1 = signal.freqz(B1, A1, worN=len(irfft), whole=True)
+W2, H2 = signal.freqz(B2, A2, worN=len(irfft), whole=True)
+# (i) Matlab calcula las freq físicas si se le indica la FS, pero 
+#     Scipy no usa la FS. Hay que calcular aparte las freq físicas:
+W1 = W1 / (2*np.pi) * FS
+W2 = W2 / (2*np.pi) * FS
+
 ##
 ## %%% apply band pass filter to inverse magnitude
 ## invirfftmag  = abs(invirfft).*abs(H1)'.*abs(H2)';
-##
+invirfftmag  = abs(invirfft) * abs(H1) * abs(H2)
+##                                                                   ^--(HASTA AQUI OK)
 ## %%% get inverse phase
 ## invirfftphase = angle(invirfft);
 ##
