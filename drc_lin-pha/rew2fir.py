0db96c4193a8a40fea8669c71a6aebdea9cd6fd1
Rsantct <rsantct@gmail.com>
2018-09-02 21:17:07 +0200

    Update rew2fir.py

--- drc_lin-pha/rew2fir.py
+++ drc_lin-pha/rew2fir.py
@@ -1,12 +1,22 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
+    v0.2
     Genera FIRs para DRC a partir de los filtros paramétricos
     de archivos .txt provinientes de Room EQ Wizard.
+    
+    Uso:
+            rew2fir.py archivoREW.txt [FS] [xK]
+            FS: por defecto 44100
+            xK: por defecto 32K taps
     
     Experimental: Además de FIR min-phase, también genera
                   el equivalente linear-phase con la misma
                   respuesta en magnitud.
+
+    Es interesante probar distintos valores fs, taps, betas de kaiser...
+    Si aumentamos beta disminuyen los microartefactos del GD del filtro, ¿audibles?,
+    pero a costa de menos resolución en la curva de filtrado en graves.
 
     NOTA: se necesita tener instalado el paquete 'AudioHumLab/audiotools'
 """
@@ -25,61 +35,66 @@ except:
     raise ValueError("rew2fir.py necesita https://githum.com/AudioHumLab/audiotools")
     sys.exit()
     
-# PARAMETROS GLOBALES:
+# PARAMETROS GLOBALES POR DEFECTO:
 fs = 44100  # Frecuencia de muestreo
 m  = 2**15  # Longitud del impulso FIR
             # (i) Para filtros de alto Q > 5  y baja frecuencia <50Hz
             #     usar m >= 16K para un FIR largo.
 
-for channel in "L", "R":
+# 0. LECTURA DEL ARCHIVO .txt de filtros paramétricos de REW
+if len(sys.argv) == 1:
+    print __doc__
+    sys.exit() 
+rewfname = sys.argv[1]
+for opc in sys.argv[1:]:
+    if opc.isdigit():
+        fs = int(opc)
+    if "K" in opc.upper():
+        k = int( opc.upper().replace("K", "") )
+        m = 1024 * k
     
-    print channel + ":"
+# 1. Partimos de una delta (espectro plano)
+imp = pydsd.delta(m)
 
-    # 0. Partimos de una delta (espectro plano)
-    imp = pydsd.delta(m)
+# 2. Aplicamos una curva Room Gain +6dB
+#gain = 6.0
+#imp = utils.RoomGain2impulse(imp, fs, gain)
 
-    # 1. Aplicamos una curva Room Gain +6dB
-    #gain = 6.0
-    #imp = utils.RoomGain2impulse(imp, fs, gain)
+# 3. Leemos los filtros paramétricos desde un archivo de texto de REW:
+PEQs = utils.read_REW_EQ_txt(rewfname)
 
-    # 2. Leemos los filtros paramétricos desde un archivo de texto de REW:
-    rewfname = "rew_" + channel + ".txt"
-    PEQs = utils.read_REW_EQ_txt(rewfname)
+# 4. Encadenamos los filtros 'peakingEQ' 
+for peqId, params in PEQs.items():
 
-    # 3. Encadenamos los filtros 'peakingEQ' 
-    for peqId, params in PEQs.items():
+    # Printado de paramétricos:
+    print  ("  fc: "    + str(   int(params['fc']      ) ) ).ljust(12) + "   " + \
+             ("Q: "     + str( round(params['Q'], 2    ) ) ).ljust(12) + "   " + \
+             ("dB: "    + str( round(params['gain'], 1 ) ) ).ljust(12) + "   " + \
+             "(BWoct: " + str( round(params['BW'], 3 ) )               + ")"
 
-        # Printado de paramétricos:
-        print  ("  fc: "    + str(   int(params['fc']      ) ) ).ljust(12) + "   " + \
-                 ("Q: "     + str( round(params['Q'], 2    ) ) ).ljust(12) + "   " + \
-                 ("dB: "    + str( round(params['gain'], 1 ) ) ).ljust(12) + "   " + \
-                 "(BWoct: " + str( round(params['BW'], 3 ) )               + ")"
+    # Calculamos los coeff IIR correspondientes:
+    b, a = pydsd.biquad(fs     = fs, 
+                        f0     = params['fc'], 
+                        Q      = params['Q'], 
+                        dBgain = params['gain'],
+                        type   = "peakingEQ"
+                       ) 
 
-        # Calculamos los coeff IIR correspondientes:
-        b, a = pydsd.biquad(fs     = fs, 
-                            f0     = params['fc'], 
-                            Q      = params['Q'], 
-                            dBgain = params['gain'],
-                            type   = "peakingEQ"
-                           ) 
+    # Aplicamos los coeff a nuestro impulso de partida
+    imp = signal.lfilter(b, a, imp)
 
-        # Aplicamos los coeff a nuestro impulso de partida
-        imp = signal.lfilter(b, a, imp)
+# 5. Guardamos el resultado (que es minimum phase)
+pcmname_mp = "mp-" + rewfname.replace('.txt', '.pcm')
+utils.savePCM32(imp, pcmname_mp)
 
-    # 4. Guardamos el resultado (que es minimum phase)
-    pcmname = "mp-" + rewfname.replace('.txt', '.pcm')
-    utils.savePCM32(imp, pcmname)
+# 6. Convertimos a LP linear phase (experimental) ...
+imp = utils.MP2LP(imp, windowed=True, kaiserBeta=1)
 
-    # 5. Convertimos a LP linear phase (experimental) ...
-    imp = utils.MP2LP(imp, windowed=True, kaiserBeta=1)
+# 7. Guardamos el resultado LP
+pcmname_lp = pcmname_mp.replace('mp-', 'lp-')
+utils.savePCM32(imp, pcmname_lp)
 
-    # 6. Guardamos el resultado LP
-    utils.savePCM32(imp, pcmname.replace('mp-', 'lp-'))
+print "Guardados FIRs: ", pcmname_mp, pcmname_lp
 
-print
-print "(i) Observar los resultados haciendo zoom con"
-print "    'IRs_viewer.py  mp-rew_R.pcm  lp-rew_R.pcm  44100'"
-print
-print "    Es interesante probar distintos valores fs, taps, betas de kaiser..."
-print "    Si aumentamos beta disminuyen los microartefactos del GD del filtro, ¿audibles?,"
-print "    pero a costa de menos resolución en la curva de filtrado en graves."
+# 8. Veamos los FIRs de EQ:
+os.system("IRs_viewer.py " + pcmname_mp + " " + pcmname_lp + " 20-20000 -eq -1 " + str(int(fs)))
