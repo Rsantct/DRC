75f1ce4fb63bea602f17e69f5c5f352e92bb4a00
Rsantct <rsantct@gmail.com>
2018-10-02 17:47:15 +0200

    Add files via upload

--- drc_lin-pha/numeraDRCs.sh
+++ drc_lin-pha/numeraDRCs.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-# renombra los drc-X-.......pcm con un número en lugar e X
+# Numera los drc-X... con el número indicado para los _lp_
+# y uno más para los _mp_
+
 
 if [[ $1 ]]; then
+    n1=$1
+    n2=$(( n1+1 ))
     for x in drc*; do
-        mv $x ${x/X/$1}
+        if [[ "$x" == *"_lp_"* ]];then
+            mv $x ${x/X/$n1}
+        else
+            mv $x ${x/X/$n2}
+        fi
     done
 else
     echo "indicar el numero que debe sustiruir a drc-X-......"
