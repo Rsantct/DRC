a3aca068c93240be0d9b75ade4255fd947d2dbdc
Rsantct <rsantct@gmail.com>
2018-10-05 21:46:31 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -13,8 +13,10 @@ rm -f ~/master.zip*
 rm -rf ~/DRC
 mv ~/DRC-master ~/DRC
 
-chmod +x ~/DRC/drc_linpha/*py
-chmod +x ~/DRC/logsweep2TF/*py
+chmod +x ~/DRC/drc_linpha/*.py
+chmod +x ~/DRC/drc_linpha/*.sh
+chmod +x ~/DRC/logsweep2TF/*.py
+chmod +x ~/DRC/logsweep2TF/*.sh
 
 rm ./update.sh  > /dev/null 2>&1
 
