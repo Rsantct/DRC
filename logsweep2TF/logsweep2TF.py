58efa09adcc11ddd2a6e7f6573538c52584979f1
Rsantct <rsantct@gmail.com>
2018-10-28 15:49:50 +0100

    Add files via upload

--- logsweep2TF/logsweep2TF.py
+++ logsweep2TF/logsweep2TF.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 """
     logsweep2TF.py v0.1
--- logsweep2TF/roommeasure.py
+++ logsweep2TF/roommeasure.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 """
     Script para obtenerla respuesta estacionaria de una sala,
