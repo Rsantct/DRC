add5d9c07d325c7c9e8d0cd08932da1d53fba71c
Rsantct <rsantct@gmail.com>
2020-08-28 13:40:35 +0200

    Update logsweep2TF.py

--- logsweep2TF.py
+++ logsweep2TF.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python3
+
+# Copyright (c) 2019 Rafael Sánchez
+# This file is part of 'Rsantct.DRC', yet another DRC FIR toolkit.
+#
+# 'Rsantct.DRC' is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# 'Rsantct.DRC' is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with 'Rsantct.DRC'.  If not, see <https://www.gnu.org/licenses/>.
+
 """
     logsweep2TF.py v0.1p3 (python3)
 
