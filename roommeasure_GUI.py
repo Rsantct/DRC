1d7f85221cb4c8bf2e26f3aca668c671416c9baf
Rsantct <rsantct@gmail.com>
2020-10-30 01:00:49 +0100

    Update roommeasure_GUI.py

--- roommeasure_GUI.py
+++ roommeasure_GUI.py
@@ -17,7 +17,7 @@ class RoommeasureGUI():
         self.root.title('DRC/roommeasure.py GUI')
         content =  ttk.Frame( root, padding=(10,10,12,12) )
 
-        # CAP PBK DEVICES
+        # SOUND CARD
         lbl_scard      = ttk.Label(content, text='SOUND CARD:')
         lbl_cap        = ttk.Label(content, text=' IN:')
         self.combo_cap = ttk.Combobox(content, values=cap_devs)
@@ -37,12 +37,6 @@ class RoommeasureGUI():
         lbl_scho         = ttk.Label(content, text='Schoeder freq:')
         self.ent_scho    = ttk.Entry(content,                     width=5)
 
-        self.combo_fs.set('48000')
-        self.combo_ch.set('LR')
-        self.combo_meas.set('3')
-        self.combo_sweep.set(str(2**17))
-        self.ent_scho.insert(0, '200')
-
         # REMOTE JACK SECTION
         lbl_rjack        = ttk.Label(content, text='Remote JACK:')
         lbl_rjaddr       = ttk.Label(content, text='addr:')
@@ -51,50 +45,61 @@ class RoommeasureGUI():
         self.ent_rjuser  = ttk.Entry(content,                     width=12)
         self.ent_rjuser.insert(0, 'paudio')
 
-
-        # BOTTOM MESSAGES AREA
-        self.lbl_msg     = ttk.Label(content, text='...', font=(None, 30))
-
         # [Run] BUTTON AREA
         lbl_run          = ttk.Label(content, text='RUN:')
+        lbl_timer        = ttk.Label(content, text='auto timer (s):')
+        self.combo_timer = ttk.Combobox(content, values=timers, width=7)
         self.noBeep      = BooleanVar()
         self.chk_noBeep  = ttk.Checkbutton(content, text=' no beep',
                                            variable=self.noBeep,
                                            onvalue=True, offvalue=False)
         self.btn_go    = ttk.Button(content, text='Go!', command=self.go)
 
+        # BOTTOM MESSAGES AREA
+        self.lbl_msg     = ttk.Label(content, text='...', font=(None, 30))
+
+        # DEFAULT VALUES
+        self.combo_fs.set('48000')
+        self.combo_ch.set('LR')
+        self.combo_meas.set('3')
+        self.combo_sweep.set(str(2**17))
+        self.ent_scho.insert(0, '200')
+        self.combo_timer.set('manual')
+
         # GRID ARRANGEMENT
-        content.grid(           row=0, column=0, sticky=(N, S, E, W) )
+        content.grid(           row=0,  column=0, sticky=(N, S, E, W) )
 
-        lbl_scard.grid(         row=0, column=0, sticky=W )
-        lbl_cap.grid(           row=1, column=0, sticky=E )
-        self.combo_cap.grid(    row=1, column=1)
-        lbl_pbk.grid(           row=1, column=2, sticky=E )
-        self.combo_pbk.grid(    row=1, column=3)
-        lbl_fs.grid(            row=1, column=4, sticky=E )
-        self.combo_fs.grid(     row=1, column=5)
+        lbl_scard.grid(         row=0,  column=0, sticky=W )
+        lbl_cap.grid(           row=1,  column=0, sticky=E )
+        self.combo_cap.grid(    row=1,  column=1)
+        lbl_pbk.grid(           row=1,  column=2, sticky=E )
+        self.combo_pbk.grid(    row=1,  column=3)
+        lbl_fs.grid(            row=1,  column=4, sticky=E )
+        self.combo_fs.grid(     row=1,  column=5)
 
-        lbl_meastitle.grid(     row=2, column=0, sticky=W )
-        lbl_ch.grid(            row=3, column=0, sticky=E )
-        self.combo_ch.grid(     row=3, column=1, sticky=W )
-        lbl_meas.grid(          row=3, column=2, sticky=E )
-        self.combo_meas.grid(   row=3, column=3, sticky=W )
-        lbl_sweep.grid(         row=3, column=4, sticky=E )
-        self.combo_sweep.grid(  row=3, column=5, sticky=W )
-        lbl_scho.grid(          row=4, column=4, sticky=E )
-        self.ent_scho.grid(     row=4, column=5, sticky=W )
+        lbl_meastitle.grid(     row=2,  column=0, sticky=W )
+        lbl_ch.grid(            row=3,  column=0, sticky=E )
+        self.combo_ch.grid(     row=3,  column=1, sticky=W )
+        lbl_meas.grid(          row=3,  column=2, sticky=E )
+        self.combo_meas.grid(   row=3,  column=3, sticky=W )
+        lbl_sweep.grid(         row=3,  column=4, sticky=E )
+        self.combo_sweep.grid(  row=3,  column=5, sticky=W )
+        lbl_scho.grid(          row=4,  column=4, sticky=E )
+        self.ent_scho.grid(     row=4,  column=5, sticky=W )
 
-        lbl_rjack.grid(         row=5, column=0, sticky=W )
-        lbl_rjaddr.grid(        row=6, column=0, sticky=E )
-        self.ent_rjaddr.grid(   row=6, column=1, sticky=W )
-        lbl_rjuser.grid(        row=6, column=2, sticky=E )
-        self.ent_rjuser.grid(   row=6, column=3, sticky=W )
+        lbl_rjack.grid(         row=5,  column=0, sticky=W )
+        lbl_rjaddr.grid(        row=6,  column=0, sticky=E )
+        self.ent_rjaddr.grid(   row=6,  column=1, sticky=W )
+        lbl_rjuser.grid(        row=6,  column=2, sticky=E )
+        self.ent_rjuser.grid(   row=6,  column=3, sticky=W )
 
-        lbl_run.grid(           row=7, column=0, sticky=W )
-        self.chk_noBeep.grid(   row=7, column=4 )
-        self.btn_go.grid(       row=7, column=5 )
+        lbl_run.grid(           row=7,  column=0, sticky=W )
+        lbl_timer.grid(         row=8,  column=2, sticky=E )
+        self.combo_timer.grid(  row=8,  column=3, sticky=W )
+        self.chk_noBeep.grid(   row=8,  column=4 )
+        self.btn_go.grid(       row=8,  column=5 )
 
-        self.lbl_msg.grid(      row=8, column=0, columnspan=6, sticky=W )
+        self.lbl_msg.grid(      row=9,  column=0, columnspan=6, sticky=W )
 
         # RESIZING BEHAVIOR
         root.rowconfigure(      0, weight=1)
@@ -154,16 +159,16 @@ class RoommeasureGUI():
         rm.do_meas_loop()
 
         # COMPUTE the average from all raw measurements
+        self.lbl_msg['text'] = 'COMPUTING AVERAGES ...'
         rm.do_averages()
-
-        # SAVING averages
         rm.do_save_averages()
 
         # Plotting prepared curves
+        self.lbl_msg['text'] = 'GRAPHS ...'
         rm.LS.plt.show()
 
         # END
-        self.lbl_msg['text'] = '- DONE -'
+        self.lbl_msg['text'] = 'END.'
 
 
 if __name__ == '__main__':
@@ -176,6 +181,7 @@ if __name__ == '__main__':
     channels = ['C', 'L', 'R', 'LR']
     takes    = list(range(1,21))
     sweeplen = [2**14, 2**15, 2**16, 2**17]
+    timers   = ['1','2','3','4','5','manual']
 
     root = Tk()
     app = RoommeasureGUI(root)
