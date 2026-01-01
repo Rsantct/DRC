84762cc95b47f9706149fe31bba3f72486c92c20
Rsantct <rsantct@gmail.com>
2026-01-01 16:43:10 +0100

    Update filter2peq.py

--- DRC/filter2peq.py
+++ DRC/filter2peq.py
@@ -64,110 +64,9 @@ import  sys
 import  json
 import  yaml
 import  numpy as np
-import  scipy.signal    as      signal
 from    scipy.optimize  import  minimize, least_squares, differential_evolution
-from    scipy.io        import  wavfile
-import  matplotlib.pyplot as    plt
 from    fmt import Fmt
-
-VALID_FS = (44100, 88200, 48000, 96000, 192000)
-
-
-def detect_channel_from_filter_filename():
-    """ returns 'L', 'R' or 'chX'
-    """
-
-    ch = ''
-
-    tmp = set_name.split('.')
-
-    if 'L' in tmp:
-        ch = 'L'
-    elif 'R' in tmp:
-        ch = 'R'
-
-    if not ch:
-
-        tmp = set_name.split('_')
-
-        if 'L' in tmp:
-            ch = 'L'
-        elif 'R' in tmp:
-            ch = 'R'
-        else:
-            ch = 'chX'
-
-    return ch
-
-
-def get_PEQ_mag(freq, fc, Q, gain_db, fs):
-    """ Calculate the magnitude curve in dB of a Peaking EQ filter.
-
-        freq: a dense, preferable logarithmic frequency axis to render the curve
-    """
-
-    # A must be lineal
-    A = 10**(gain_db / 40)
-    w0 = 2 * np.pi * fc / fs
-    alpha = np.sin(w0) / (2 * Q)
-
-    # Filter coeffs (RBJ Cookbook)
-    b = [1 + alpha * A, -2 * np.cos(w0), 1 - alpha * A]
-    a = [1 + alpha / A, -2 * np.cos(w0), 1 - alpha / A]
-
-    w_eval = 2 * np.pi * freq / fs
-    _, h = signal.freqz(b, a, worN=w_eval)
-
-    return 20 * np.log10( np.maximum(np.abs(h), 1e-5) )
-
-
-def get_PEQ_pha(freq, fc, Q, gain_db, fs):
-    """ Calculate the phase curve of a Peaking EQ filter, in degrees.
-
-        freq: a dense, preferable logarithmic frequency axis to render the curve
-    """
-    A = 10**(gain / 40)
-    w0 = 2 * np.pi * fc / fs
-    alpha = np.sin(w0) / (2 * Q)
-
-    b = [1 + alpha * A, -2 * np.cos(w0), 1 - alpha * A]
-    a = [1 + alpha / A, -2 * np.cos(w0), 1 - alpha / A]
-
-    w_eval = 2 * np.pi * freq / fs
-    _, h = signal.freqz(b, a, worN=w_eval)
-
-    return np.degrees( np.angle(h) )
-
-
-def get_PEQs_mag(freq, peq_list, fs):
-    """ Calculate the total magnitude response curve accumulated
-        from a list of PEQ filters, in dB.
-
-        freq: a dense, preferable logarithmic frequency axis to render the curve
-    """
-
-    mag = np.zeros_like(freq)
-
-    for peq in peq_list:
-        fc, Q, gain = peq['fc'], peq['q'], peq['gain']
-        mag += get_PEQ_mag(freq, fc, Q, gain, fs)
-
-    return mag
-
-
-def get_PEQs_pha(freq, peq_list, fs):
-    """ Calculate the total phase curve accumulated
-        from a list of PEQ filters, in degrees.
-
-        freq: a dense, preferable logarithmic frequency axis to render the curve
-    """
-    phase = np.zeros_like(freq)
-
-    for peq in peq_list:
-        fc, Q, gain = peq['fc'], peq['q'], peq['gain']
-        phase += get_PEQ_mag(freq, fc, Q, gain, fs)
-
-    return phase
+import  common as cm
 
 
 def objective_function(params, f_target, m_target, fs, num_peqs):
@@ -176,7 +75,7 @@ def objective_function(params, f_target, m_target, fs, num_peqs):
     total_mag = np.zeros_like(f_target)
     for i in range(num_peqs):
         fc, Q, gain = params[i*3 : (i+1)*3]
-        total_mag += get_PEQ_mag(f_target, fc, Q, gain, fs)
+        total_mag += cm.get_PEQ_mag(f_target, fc, Q, gain, fs)
 
     # --- WEIGHTS: The core of low-frequency resolution ---
     # A vector of weights that decays with frequency
@@ -195,7 +94,7 @@ def objective_function_ultra_bass(params, f_target, m_target, fs, num_peqs):
     total_mag = np.zeros_like(f_target)
     for i in range(num_peqs):
         fc, Q, gain = params[i*3 : (i+1)*3]
-        total_mag += get_PEQ_mag(f_target, fc, Q, gain, fs)
+        total_mag += cm.get_PEQ_mag(f_target, fc, Q, gain, fs)
 
     # Peso hiper-agresivo: decaimiento potencial de la importancia
     # 20Hz tendrá un peso de (20000/20)^1.2 = 4000 aprox.
@@ -216,7 +115,7 @@ def residuals(params, f, target, fs, num_peqs, weights):
 
     for i in range(num_peqs):
         fc, Q, gain = params[i*3 : (i+1)*3]
-        model += get_PEQ_mag(f, fc, Q, gain, fs)
+        model += cm.get_PEQ_mag(f, fc, Q, gain, fs)
 
     return (model - target) * weights
 
@@ -276,8 +175,8 @@ def get_optimized_peqs_from_frd(frd, fs, num_peqs):
                 }
 
 
-            mag_calc  = get_PEQs_mag(freq, eq_config['filters'], fs)
-            # pha_calc = get_PEQs_pha(freq, eq_config['filters'], fs)  # unused
+            mag_calc  = cm.get_PEQs_mag(freq, eq_config['filters'], fs)
+            # pha_calc = cm.get_PEQs_pha(freq, eq_config['filters'], fs)  # unused
 
             metrics = calculate_metrics(freq, target, mag_calc)
 
@@ -578,191 +477,6 @@ def get_optimized_peqs_from_frd(frd, fs, num_peqs):
     return eq_config
 
 
-def visualize_eq_results(frd, peq_set):
-    """ Compare and plot both responses:
-            frd:        a magnitude vs freq response curve
-            peq_set:    a set o Peaking EQ that emulates 'frd'
-    """
-
-    def get_points_peq(peqs):
-
-        f = []
-        m  = []
-        for p in peqs:
-            f.append(p['fc'])
-            m.append(p['gain'])
-
-        return f, m
-
-
-    # the span in dB for the Error graph
-    db_error_span = 5.0
-
-    # extract freq and mag dB from the given frd 2 columns array
-    freq  = frd[:, 0]
-    mag   = frd[:, 1]
-
-    # Fs and peaking EQ filters
-    fs    = peq_set.get('metadata', {}).get('fs', 0)
-    peqs  = peq_set["filters"]
-
-    # 2. Reconstruir la respuesta total
-    # A dense, logarithmic frequency axis for the graph
-    f_plot  = np.geomspace(20, 20000, 1000)
-    mag_peq = get_PEQs_mag(f_plot, peqs, fs)
-
-    peqs_f, peqs_m = get_points_peq(peqs)
-
-
-    # 3. Interpolar la original para calcular el error exacto en f_plot
-    mag_interp = np.interp(f_plot, freq, mag)
-    error = mag_peq - mag_interp
-
-    # 4. Crear la visualización OJO se comparte el eje X
-    fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 8), sharex=False,
-                                   gridspec_kw={'height_ratios': [3, 1]})
-
-    # Gráfica Principal: Magnitud
-    ax1.semilogx(peqs_f, peqs_m,  'o', label=f'PEQs (tot {len(peqs_f)})', color='black')
-    ax1.semilogx(freq,   mag,     label='Target curve',  color='gray', alpha=0.5, linewidth=2)
-    ax1.semilogx(f_plot, mag_peq, label='PEQ emulation', color='blue', linestyle='--')
-    ax1.set_xlim(20, 20000)
-    ax1.set_xticks([20, 50, 100, 500, 1000, 5000, 10000, 20000])
-    ax1.set_xticklabels([20, 50, 100, 500, 1000, 5000, 10000, 20000])
-    ax1.tick_params(labelbottom=True)
-    ax1.set_ylim(-36, 12)
-    ax1.set_xlabel('Freq (Hz)')
-    ax1.set_ylabel('Gain (dB)')
-    tmp = ' auto' if not mag_offset else ''
-    ax1.set_title(f'PEQ emulation ({optimizer}) (ch: {ch}, offset {moved_dB} dB{tmp})')
-    ax1.grid(True, which="both", linestyle="-", alpha=0.3)
-    ax1.legend()
-
-    # Gráfica de Error (Residuo)
-    ax2.semilogx(f_plot, error, color='red', linewidth=1)
-    ax2.set_xlim(20, 20000)
-    ax2.tick_params(labelbottom=False)
-    ax2.set_ylim([-db_error_span, db_error_span])
-    ax2.set_ylabel('Error (dB)')
-    ax2.fill_between(f_plot, error, color='red', alpha=0.1)
-    ax2.grid(True, which="both", linestyle="-", alpha=0.3)
-    ax2.set_title('Residual error', fontsize=10)
-
-    plt.tight_layout()
-
-    # will be dumped after closing plots
-    plt.savefig(f'{json_path}.png')
-
-    if do_plot:
-        plt.show()
-    else:
-        plt.close('all')
-
-
-def load_wav(filepath, ch, normalize = True):
-
-    if ch == 'L':
-        ch = 0
-    elif ch == 'R':
-        ch = 1
-    else:
-        ch = int(ch)
-
-    fs, data = wavfile.read(filepath)
-
-    if len(data.shape) == 1:
-        fir_coeffs = data
-    else:
-        fir_coeffs = data[:, ch]
-
-    if normalize:
-
-        if data.dtype == np.int16:
-            fir_coeffs = fir_coeffs / 32768.0
-
-        elif data.dtype == np.int32:
-            fir_coeffs = fir_coeffs / 2147483648.0
-
-    return fir_coeffs, fs
-
-
-def load_fir_file(fir_path, ch):
-    """ 'ch' only used for wav channel selection
-    """
-
-    # fs will be set from the wav file if so
-    global fs
-
-    fname, fext = os.path.splitext( os.path.basename(fir_path) )
-
-    if fext == '.wav':
-
-        h, fs = load_wav(fir_path, ch)
-
-    else:
-        # for PCM raw FIRs, fs must be explicited at command line
-        if not fs in VALID_FS:
-            print(__doc__)
-            print(f'FS must be in {VALID_FS}')
-            sys.exit()
-
-        with open(fir_path, 'rb') as f:
-            h = np.fromfile(f, dtype=np.float32)
-
-    return h
-
-
-def fir2frd(h, fs, freq=None):
-
-    if not freq:
-        n_points = 1000
-        freq = np.logspace(np.log10(10), np.log10(fs / 2), n_points)
-    else:
-        n_points = freq.size
-
-    w_rad = 2 * np.pi * freq / fs
-
-    w, h_resp = signal.freqz(h, worN=w_rad)
-
-    mag_db  = 20 * np.log10(np.abs(h_resp))
-
-    pha_deg = np.angle(h_resp, deg=True)
-
-    return np.column_stack((freq, mag_db, pha_deg))
-
-
-def get_avg_flat_region(frd, hz_ini=300, hz_end=3000):
-    """
-        <frd>   a 2D columns array  [ Hz : dB ]
-
-        returns the avg flat region value in dB into the given freq range
-    """
-
-    # Create 500 log points from  `hz_ini` to `hz_end`
-    hz_interp = np.geomspace(hz_ini, hz_end, 100)
-
-    # Interpolate the dB values ​​for those new points
-    hz = frd[:,0]
-    db = frd[:,1]
-
-    # As log spaced audio freq points can be widely separated, it is
-    # preferred to interpolate over the logarithm of the frequency.
-    db_interp = np.interp(np.log10(hz_interp), np.log10(hz), db)
-
-    # Average the interpolated values
-    avg = np.mean(db_interp)
-
-    return avg
-
-
-def move_flat_region(frd):
-
-    flat_offset_dB = get_avg_flat_region(frd, 200, 4000)
-    frd[:, 1] -= flat_offset_dB
-
-    return frd, round(-flat_offset_dB, 2)
-
-
 if __name__ == "__main__":
 
     optimizer = 'minimize'
@@ -856,9 +570,9 @@ if __name__ == "__main__":
 
         if os.path.isfile( fir_path ):
 
-            fir = load_fir_file( fir_path, ch )
+            fir, fs = cm.load_fir_file( fir_path, ch, fs )
 
-            frd = fir2frd(fir, fs)
+            frd = cm.fir2frd(fir, fs)
 
             json_dir  = os.path.dirname(fir_path)
             set_name  = os.path.splitext( os.path.basename(fir_path) )[0]
@@ -875,13 +589,13 @@ if __name__ == "__main__":
 
 
     # Check FS
-    if not fs in VALID_FS:
-        print(f'FS must be in {VALID_FS}')
+    if not fs in cm.VALID_FS:
+        print(f'FS must be in {cm.VALID_FS}')
         sys.exit()
 
     # Check channel
     if not ch:
-        ch = detect_channel_from_filter_filename()
+        ch = cm.detect_channel_from_set_name(set_name)
 
     valid_channels = ('L', 'R')
 
@@ -895,7 +609,8 @@ if __name__ == "__main__":
         frd[:, 1] += mag_offset
         moved_dB = mag_offset
     else:
-        frd, moved_dB = move_flat_region(frd)
+        # auto detect flat region
+        frd, moved_dB = cm.move_flat_region(frd)
 
 
     ########################
@@ -913,4 +628,6 @@ if __name__ == "__main__":
         json.dump(peq_config, f, indent=4, ensure_ascii=False)
 
     # Graph results vs original curve
-    visualize_eq_results(frd, peq_config)
+    cm.plot_frd_vs_peqs(frd, moved_dB, peq_config['filters'],
+                        fs, ch=ch, emulation_method=optimizer,
+                        png_path=json_path, do_plot=do_plot)
